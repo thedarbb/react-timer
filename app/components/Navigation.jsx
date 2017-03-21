e7c5d6f544062d741cefa46a6d3928fa3779be97
Dustin <dustin.sappier@bluespurs.com>
2017-03-21 16:25:27 -0300

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Time App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/thedarbb" target="_blank">Dustin Sappier</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
+module.exports = Navigation;
