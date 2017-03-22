721e922756c8bde636d36f1bdb0725d1e8bf7256
Dustin <dustin.sappier@learningspurs.com>
2017-03-22 13:26:49 -0300

    added karma - mocha for unit testing

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.spec.jsx'],
+    preprocessors: {
+      'app/tests/**/*.spec.jsx': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
